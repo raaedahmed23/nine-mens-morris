f221b7f965d77c4fb4e2832c25638140cb3aa84e
raaedahmed23 <raaedahmed23@gmail.com>
2023-03-14 19:18:33 -0500

    function declarations

---
+++ Part1/MiniMaxOpening.py
@@ -0,0 +1,29 @@
+def GenerateAdd(): 
+    pass
+
+def GenerateMove():
+    pass
+
+def GenerateRemove():
+    pass
+
+def GenerateHopping():
+    pass
+
+def neighbors():
+    pass
+
+def closeMill():
+    pass
+
+def StaticEstimateOpening():
+    pass
+
+def StaticEstimateMidEnd():
+    pass
+
+def GenerateMovesOpening():
+    pass
+
+def GenerateMovesMidEnd():
+    pass
