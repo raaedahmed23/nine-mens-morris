07781919a4e6dc529bcce5326807e0ac9db5c900
raaedahmed23 <raaedahmed23@gmail.com>
2023-03-28 23:37:19 -0500

    min max black opening draft

---
+++ MiniMaxOpeningBlack.py
@@ -0,0 +1,74 @@
+import sys 
+from MiniMaxOpening import GenerateMovesOpeningBlack, GenerateMovesOpening, StaticEstimateOpening, state, reverse
+
+MIN = -9000000000000
+MAX = 9000000000000
+
+class Solution:
+    def __init__(self, board = '', depth = 0):
+        self.curr_board = board
+        self.depth = depth
+
+        def write_result(board):
+            with open(sys.argv[2], 'w') as f:
+                for line in board: 
+                    f.write(line)
+
+        def minmaxopening(depth, board, label):
+            exit = state()
+            entry = state()
+
+            if label == 0:
+                move_list = GenerateMovesOpeningBlack(board)
+                exit.estimate = MAX
+            else:
+                move_list = GenerateMovesOpening(board)
+                exit.estimate = MIN
+
+            if depth == 0: 
+                counter_final = StaticEstimateOpening(board)
+                exit = state(counter_final, exit.counter + 1, board)
+                return exit
+            
+
+            for move in move_list:
+                if label == 0: 
+                    entry = minmaxopening(depth-1, move, 1)
+                    if entry.estimate < exit.estimate:
+                        exit.board = move
+                        exit.estimate = entry.estimate
+                    
+                    exit.counter += entry.counter
+
+                else:
+                    entry = minmaxopening(depth - 1, move, 0)
+                    if entry.estimate > exit.estimate:
+                        exit.board = move
+                        exit.estimate = entry.estimate
+                    
+                    exit.counter += entry.counter
+                
+            return exit
+
+        def run():
+            result = minmaxopening(self.depth, self.curr_board, 1)
+            result.board = reverse(result.board)
+            print('Board Position: ', result.board)
+            print('Positions Evaluated by Static Estimaton:', result.counter)
+            print('MINIMAX Estimate:', result.estimate)
+            write_result(result.board)
+
+        run()
+
+
+if __name__ == '__main__':
+    board = ''
+    with open(sys.argv[1], 'r') as f: 
+        for line in f:
+            board = line
+
+    rev_board = reverse(board)
+    depth = int(sys.argv[3])
+    Solution(rev_board, depth)
+
+    
